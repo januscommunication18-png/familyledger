92009534079a1b3f29c595f412371bc9f8d2c05f
faizanhumayun <32742863+faizanhumayun@users.noreply.github.com>
2026-01-30 01:13:08 +0500

    search issue

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -14,9 +14,6 @@
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-    <!-- Alpine.js -->
-    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
-
     <!-- Pushed Styles -->
     @stack('styles')
 
